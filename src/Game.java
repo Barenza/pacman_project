ba57fb629e056d7732a0f72f76bbabeafa438e48
Barenza <leimer.c@outlook.de>
2025-04-16 09:50:57 +0200

    map render

--- src/Game.java
+++ src/Game.java
@@ -5,10 +5,13 @@ public class Game extends JFrame {
 
     private final Display display;
 
+    private final GameMap map;
+
     public Game() {
         super("Game");
 
         display = new Display(this);
+        map = new GameMap(40);
 
         setSize(1080, 720);
         setResizable(false);
@@ -22,6 +25,8 @@ public class Game extends JFrame {
     public void render(Graphics2D g2) {
         g2.setColor(Color.BLACK);
         g2.fillRect(0, 0, getWidth(), getHeight());
+
+        map.render(g2, map.getTileSize());
     }
 
     public static void main(String[] args) {
