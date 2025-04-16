e85a501a7564f03142c9c5c8054c14262e7cd90b
Barenza <leimer.c@outlook.de>
2025-04-16 21:31:59 +0200

    Player final build

--- src/game/objects/creatures/Player.java
+++ src/game/objects/creatures/Player.java
@@ -1,20 +1,53 @@
 package game.objects.creatures;
 
 import game.Game;
+import game.objects.creatures.enemy.Enemy;
+import game.objects.tiles.Air;
+import game.objects.tiles.Dot;
+
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.geom.Ellipse2D;
 
 public class Player extends Creature implements KeyListener {
-
     public Player(Game game, double centerX, double centerY, double radius, double speed) {
         super(game, centerX, centerY, radius, speed, Color.YELLOW);
     }
 
+    private void tickDotCollision() {
+        int x = (int) centerX;
+        int y = (int) centerY;
+
+        if (game.getMap().getTile(x, y) instanceof Dot dot) {
+            double dx = dot.getCenterX() - centerX;
+            double dy = dot.getCenterY() - centerY;
+            double r = dot.getRadius() + radius;
+
+            if (dx * dx + dy * dy < r * r) {
+                game.getMap().setTile(x, y, new Air(x, y));
+                if (game.getMap().dotCount() == 0) {
+                    game.win();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void tickPreferredDirection() {
+        for (Enemy enemy : game.getEnemies()) {
+            enemy.tickPreferredDirection();
+        }
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        tickDotCollision();
+    }
+
     @Override
     public void render(Graphics2D g, int tileSize) {
-
         double centerXOnScreen = centerX * tileSize;
         double centerYOnScreen = centerY * tileSize;
         double radiusOnScreen = radius * tileSize;
@@ -22,6 +55,20 @@ public class Player extends Creature implements KeyListener {
 
         g.setColor(color);
         g.fill(new Ellipse2D.Double(centerXOnScreen - radiusOnScreen, centerYOnScreen - radiusOnScreen, diameterOnScreen, diameterOnScreen));
+
+        // Eyes
+        Enemy closestEnemy = null;
+        double closestSqDistance = Double.MAX_VALUE;
+        for (Enemy enemy : game.getEnemies()) {
+            double difX = enemy.centerX - centerX;
+            double difY = enemy.centerY - centerY;
+            double sqDistance = difX * difX + difY * difY;
+            if (sqDistance < closestSqDistance) {
+                closestEnemy = enemy;
+                closestSqDistance = sqDistance;
+            }
+        }
+        renderEyes(g, centerXOnScreen, centerYOnScreen, radiusOnScreen, closestEnemy.centerX, closestEnemy.centerY);
     }
 
     @Override
