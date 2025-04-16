a441c6db6716b994ea06808094363059405a058d
Barenza <leimer.c@outlook.de>
2025-04-16 12:59:47 +0200

    player movement adjusted

--- src/game/objects/creatures/Player.java
+++ src/game/objects/creatures/Player.java
@@ -2,9 +2,11 @@ package game.objects.creatures;
 
 import game.Game;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.geom.Ellipse2D;
 
-public class Player extends Creature {
+public class Player extends Creature implements KeyListener {
 
     public Player(Game game, double centerX, double centerY, double radius, double speed) {
         super(game, centerX, centerY, radius, speed, Color.YELLOW);
@@ -16,9 +18,41 @@ public class Player extends Creature {
         double centerXOnScreen = centerX * tileSize;
         double centerYOnScreen = centerY * tileSize;
         double radiusOnScreen = radius * tileSize;
-        double diameterOnScreen = radius * 2.0;
+        double diameterOnScreen = radiusOnScreen * 2.0;
 
         g.setColor(color);
         g.fill(new Ellipse2D.Double(centerXOnScreen - radiusOnScreen, centerYOnScreen - radiusOnScreen, diameterOnScreen, diameterOnScreen));
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // ignore
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_W -> {
+                preferredDirectionX = 0;
+                preferredDirectionY = -1;
+            }
+            case KeyEvent.VK_A -> {
+                preferredDirectionX = -1;
+                preferredDirectionY = 0;
+            }
+            case KeyEvent.VK_S -> {
+                preferredDirectionX = 0;
+                preferredDirectionY = 1;
+            }
+            case KeyEvent.VK_D -> {
+                preferredDirectionX = 1;
+                preferredDirectionY = 0;
+            }
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // ignore
     }
 }
